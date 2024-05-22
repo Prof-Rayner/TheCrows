0936e71cb1431b484376dfa2f73c78ff3d641141
allyst <alastroboy2000@gmail.com>
2024-05-22 08:39:27 -0300

    redirecionamento
    ele redireciona a página home

--- js/alter.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("redirectButton").addEventListener("click", function() {
-        window.location.href = "youtube.html";
-    });
-});
