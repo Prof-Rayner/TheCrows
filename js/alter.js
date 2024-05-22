5ddcb44285d287b4821bd29e7b96078a11e7b029
allyst <alastroboy2000@gmail.com>
2024-05-21 23:59:52 -0300

    arquivo 'alter'
    adicionei o arquivo alter que consiste no JS em que o usuário troque a página para o retorno do jogo após a mensagem de parabéns

---
+++ js/alter.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("redirectButton").addEventListener("click", function() {
+        window.location.href = "youtube.html";
+    });
+});
