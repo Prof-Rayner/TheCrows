c7d6c9ee8933114d6ce3d4a71bc919bfe5bb7aea
otavio <164201968+taviohenrique@users.noreply.github.com>
2024-06-01 23:10:26 -0300

    pop up
    falta ajustar algumas coisinhas mas já está 90% pronto

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggle() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('activate')
+    const info = document.getElementById('info');
+    info.classList.toggle('activate')
+}
