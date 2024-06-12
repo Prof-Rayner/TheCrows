ac60de3fb38d1720e4cf30451e53c47d74c76d33
taviohenrique <taviohenrique1510@gmail.com>
2024-06-12 11:56:05 -0300

    testes
    para o popup

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ function toggle() {
     const info = document.getElementById('info');
     info.classList.toggle('activate')
 }
+
+
+
+
+// var container = document.getElementById("container-info");
+// var abrirPopup = document.getElementById("abrir");
+// var fecharPopup = document.getElementById("sair");
+
+
+// abrirPopup.onclick = function(){
+//     container.style.display = "block";
+// }
+
+// fecharPopup.onclick = function () {
+//     container.style.display = "none";
+// }
+
+
+// window.onclick = function(event) {
+//     if (event.target == blur) {
+//       blur.style.opacity = 0;
+//     }
+//   }
