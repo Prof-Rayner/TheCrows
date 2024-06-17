7061e624e47694f8aefcb037a2c1774cda75e00b
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-06-17 09:09:56 -0300

    Add. de arquivos
    adicionei uma imagem teste e o JS

--- controleRotas.php
+++ controleRotas.php
@@ -1,5 +1,22 @@
 <?php
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <script defer src="./js/rotas.js"></script>
+</head>
+
+<body>
+    <div id="imagem"></div>
+    <h1>pagina inicial</h1>
+    <audio controls id="audio">
+        <source src="./" type="audio/mpeg">
+    </audio>
+</body>
+
+</html>
---
+++ js/rotas.js
@@ -0,0 +1,46 @@
+var usuarioLogado = localStorage.getItem('usuarioLogado');
+ 
+function exibirMensagem() {
+    if(!usuarioLogado) {
+       
+        exibirImagem()
+ 
+        localStorage.setItem('usuarioLogado', true);
+    }
+}
+
+function exibirImagem() {
+    var img = new Image();
+ 
+    img.src = './GoW.jpg';
+ 
+    img.addEventListener('load', function() {
+        document.body.appendChild(img);
+    });
+ 
+    img.dispatchEvent(new Event('load'));
+}
+ 
+function timer() {
+ 
+}
+ 
+function tocarAudio() {
+    const audio = new Audio();
+    audio.src = document.querySelector('#audio').src;
+ 
+    audio.play();
+}
+ 
+function getAudioTime() {
+    const audio = document.getElementById('audio');
+    console.log(`Duração do áudio: ${audioDuration} segundos`);
+ 
+    audio.addEventListener('ended', () => {
+        const audioDuration = Math.floor(audio.duration);
+    });
+ 
+}
+ 
+exibirMensagem();
+getAudioTime();
