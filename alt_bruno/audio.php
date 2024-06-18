e1f04927372d1e288690f384e4ae3cc8286ce811
Samuel Henricky <henrickyoliveira.gon@gmail.com>
2024-06-18 08:42:21 -0300

    alterações do bruno
    Usarei o código que o bruno conseguiu avançar na ultima aula para realizar melhorias e dar continuidade no projeto

---
+++ alt_bruno/audio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <script defer src="custom.js"></script>
+</head>
+
+<body>
+    <div id="imagem"></div>
+    <h1>Home</h1>
+    <img src="./img/2563963.jpg" alt="image">
+    <audio autoplay id="audio">
+        <source src="./audios/01. God of War.mp3" type="audio/mpeg">
+ 
+    </audio>
+</body>
+
+</html>
---
+++ alt_bruno/js/custom.js
@@ -0,0 +1,34 @@
+var usuarioLogado = localStorage.getItem('usuarioLogado');
+ 
+function exibirMensagem() {
+    if(!usuarioLogado) {
+        localStorage.setItem('usuarioLogado', true);
+    }
+}
+ 
+function playsound(audioname, loop) {
+    let audio = new Audio(audioname);
+    audio.loop = loop;
+    audio.play();
+}
+ playsound("./audios/01. God of War.mp3")
+ 
+function tocarAudio() {
+    const audio = new Audio();
+    audio.src = document.querySelector('#audio').src;
+ 
+    audio.play();
+}
+ 
+function getAudioTime() {
+    const audio = document.getElementById('audio');
+    console.log(`Duração do áudio: ${audioDuration} segundos`);
+ 
+    audio.addEventListener('ended', () => {
+        const audioDuration = Math.floor(audio.duration);
+    });
+ 
+}
+ 
+exibirMensagem();
+getAudioTime();
