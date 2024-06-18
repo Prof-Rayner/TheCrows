89fc89cced1dbf3227404619bfcf38b914849644
Samuel Henricky <henrickyoliveira.gon@gmail.com>
2024-06-18 08:39:13 -0300

    código base

---
+++ unica/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <script defer src="unique.js"></script>
+</head>
+
+<body>
+    <div id="imagem"></div>
+    <h1>pagina inicial</h1>
+    <audio controls id="audio">
+        <source src="./audio/audio.mp3" type="audio/mpeg">
+
+    </audio>
+</body>
+
+</html>
