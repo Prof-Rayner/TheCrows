ebf630186d90f742d104437aa5bccb51b653ddd0
allyst <alastroboy2000@gmail.com>
2024-06-24 11:06:07 -0300

    mexendo no .htaccess (em processo)

---
+++ 404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erro</title>
+</head>
+<body>
+    <?php
+        var_dump($_GET);
+    ?>
+    <h1>Página não encontrada</h1>
+    <p><a href="index">Voltar para a página inicial</a></p>
+</body>
+</html>
